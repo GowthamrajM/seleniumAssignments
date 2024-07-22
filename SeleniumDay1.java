d0fe5458795d9f7bd8198fde2110609023594e65
GowthamrajM <81296813+GowthamrajM@users.noreply.github.com>
2024-07-22 20:22:31 +0530

    Add files via upload

---
+++ SeleniumDay1.java
@@ -0,0 +1,17 @@
+package week2.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDay1 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		
+		
+	}
+
+}
